fe113f99fa01999bc79526ed79926fcafca23449
ines.ayari@medtech.tn <77331802+Ines5Ayari@users.noreply.github.com>
2022-06-04 17:18:50 +0100

    second commit

--- Middleware.js
+++ Middleware.js
@@ -3,7 +3,7 @@ const Middleware = (req, res, next) => {
   let numberOfDate = day.getDay();
   console.log(numberOfDate);
   let time = day.getHours();
-  if (numberOfDate <= 6 && numberOfDate >= 1 && time >= 9 && time <= 17) {
+  if (numberOfDate <= 5 && numberOfDate >= 1 && time >= 9 && time <= 17) {
     next();
   } else {
     return res.status(400).json({ msg: "you are not authorized" });
